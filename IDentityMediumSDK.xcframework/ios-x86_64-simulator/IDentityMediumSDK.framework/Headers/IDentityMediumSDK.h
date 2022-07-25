9a486ca7520c613508ada0a7139030c805d9e2ad
Pankaj Sawale <pankaj.sawale@idmission.com>
2022-07-25 16:24:26 +0530

    initial commit

---
+++ IDentityMediumSDK.xcframework/ios-x86_64-simulator/IDentityMediumSDK.framework/Headers/IDentityMediumSDK.h
@@ -0,0 +1,22 @@
+//
+//  IDentityMediumSDK.h
+//  IDentityMediumSDK
+//
+//  Created by Stefan Kaczmarek on 1/16/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IDentityMediumSDK.
+FOUNDATION_EXPORT double IDentityMediumSDKVersionNumber;
+
+//! Project version string for IDentityMediumSDK.
+FOUNDATION_EXPORT const unsigned char IDentityMediumSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IDentityMediumSDK/PublicHeader.h>
+
+#import "IDentityXSLT.h"
+#import "UIImage+Rotation.h"
+#import "UIImage+Glare.h"
+#import "RSA.h"
+#import "FWEncryptorAES.h"
---
+++ README.md
@@ -0,0 +1,1 @@
+# IDentityMediumSDK2.0
